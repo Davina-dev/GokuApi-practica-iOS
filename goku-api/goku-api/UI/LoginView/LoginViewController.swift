d8d8768feb498463595c004ead124905899a5c13
Davina Medina <davinamr91@gmail.com>
2022-12-23 17:00:10 +0100

    Add style login

--- goku-api/goku-api/UI/LoginView/LoginViewController.swift
+++ goku-api/goku-api/UI/LoginView/LoginViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: NSLayoutConstraint!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
 }
