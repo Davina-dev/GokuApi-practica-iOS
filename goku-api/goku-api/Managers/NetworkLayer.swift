4b36e33a82dca8c05254aa6dec022e4066fb8a4e
Davina Medina <davinamr91@gmail.com>
2023-01-02 19:48:59 +0100

    Add transformations list functional

--- goku-api/goku-api/Managers/NetworkLayer.swift
+++ goku-api/goku-api/Managers/NetworkLayer.swift
@@ -106,6 +106,53 @@ final class NetworkLayer {
         task.resume()
     }
     
+    // repetimos lo mismo para transformaciones d heros
+    
+    func fetchTransformations(token: String?, heroeId: String?, completion: @escaping ([Transformation]?, Error?) -> Void) {
+        guard let url = URL(string: "https://dragonball.keepcoding.education/api/heros/tranformations") else {
+            completion(nil, NetworkError.malformedURL)
+            return
+        }
+        
+        var urlComponents = URLComponents()
+        urlComponents.queryItems = [URLQueryItem(name: "id", value: heroeId ?? "")]
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("Bearer \(token ?? "")", forHTTPHeaderField: "Authorization")
+        urlRequest.httpBody = urlComponents.query?.data(using: .utf8)
+        
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode
+                completion(nil, NetworkError.statusCode(code: statusCode))
+                print("Error loading URL: Status error --> ", (response as? HTTPURLResponse)?.statusCode ?? -1)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, NetworkError.noData)
+                return
+            }
+            
+            guard let transformations = try? JSONDecoder().decode([Transformation].self, from: data) else {
+                completion(nil, NetworkError.decodingFailed)
+                return
+            }
+            
+            // Yu-huu!
+            completion(transformations, nil)
+        }
+        
+        task.resume()
+    }
+    
     
     
     
